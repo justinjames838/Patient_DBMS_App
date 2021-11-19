793c702633d9eacdcd97f2224133e9d088e73f0b
Justin James <justinjames838@gmail.com>
2021-11-19 12:32:44 +0530

    Added Room Manager Pages

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from json import encoder
 import os
 import json
 from sqlalchemy import create_engine
-from flask import Flask,request,render_template,redirect,url_for,jsonify
+from flask import Flask,request,render_template,redirect,url_for,session
 
 
 def create_app(test_config=None):
@@ -15,7 +15,7 @@ def create_app(test_config=None):
     DATABASE_URI = "postgresql://postgres:root@localhost:5432/patient"
     db = create_engine(DATABASE_URI)
     
-
+    
 
 
     if test_config is None:
@@ -35,6 +35,8 @@ def create_app(test_config=None):
     # a simple page that says hello
     @app.route('/')
     def hello():
+        session["Pharmacist"] = False
+        session["Room_Manager"] = False
         return render_template("login.html")
     
     @app.route('/login',methods = ["POST","GET"])
@@ -43,7 +45,11 @@ def create_app(test_config=None):
             user = request.form.get("username")
             passwd = request.form.get("password")
             if(user=="Pharmacist" and passwd =="pharma"):
+                session["Pharmacist"] = True
                 return redirect(url_for("Pharmacist"))
+            elif(user=="Room_Manager" and passwd =="room_manager"):
+                session["Room_Manager"] = True
+                return redirect(url_for("Room_Manager"))
             else:
                 return render_template("invalid_login.html")
         else:
@@ -52,7 +58,11 @@ def create_app(test_config=None):
     @app.route('/Pharmacist',methods = ["POST","GET"])
     def Pharmacist():
         if request.method=="GET":
-            return render_template("Pharmacist.html")
+            if session["Pharmacist"] :
+                return render_template("Pharmacist.html")
+            else:
+                return render_template("login.html")
+                
 
     @app.route('/get_medicine',methods=["POST"])
     def get_medicine():
@@ -96,5 +106,101 @@ def create_app(test_config=None):
                 db.execute("update medicine set cost = "+new_cost+",info = '"+new_info+"'"+ "where name = '"+new_name+"'" )
             
             return render_template("med_insert_res.html",exists = existing)
+
+
+
+    @app.route("/Room_Manager",methods=["POST","GET"])
+    def Room_Manager():
+        if request.method=="GET":
+            if session["Room_Manager"] :
+                return render_template("Room_Manager.html")
+            else:
+                return render_template("login.html")
+
+    @app.route('/get_room',methods=["POST"])
+    def get_room():
+        if request.method=="POST":
+            data = request.form.get("room_type")
+            out = db.execute("select * from room where type="+"'"+data+"'")
+            res = [list(row) for row in out]
+
+            res = [[str(bit) for bit in item] for item in res]
+            #db.execute("insert into medicine values (999,5000.00,'Test','Test desc')")
+            res = {'data':res}
+            
+            return render_template("room_details.html",res = res)
+
+    @app.route('/add_room',methods=["POST"])
+    def add_room():
+        if request.method=="POST":
+
+            existing=0
+
+            existing_names= db.execute("select type from room")
+            existing_names = [list(row) for row in existing_names]
+            existing_names = [[str(bit) for bit in item] for item in existing_names]
+            existing_names = [item[0] for item in existing_names]
+
+            new_type = request.form.get("new_room_type")
+            new_cost = request.form.get("new_room_cost")
+            
+            if new_type in existing_names:
+                existing=1
+            
+            max_id = db.execute("select max(room_id) from room")
+            max_id = [list(row) for row in max_id]
+            max_id = [[int((str(bit))) for bit in item] for item in max_id]
+            max_id = [item[0] for item in max_id]
+
+            max_id = max_id[0] + 1
+
+            if(existing == 0):
+                db.execute("insert into room values ("+str(max_id)+",'"+new_type+"',"+new_cost+")")
+            else:
+                db.execute("update room set cost = "+new_cost+ " where type = '"+new_type+"'" )
+            
+            return render_template("room_insert_res.html",exists = existing)
+
+    @app.route('/assign_nurse',methods=["POST"])
+    def assign_nurse():
+        if request.method=="POST":
+            nurse_fname = request.form.get("nurse_fname")
+            nurse_mname = request.form.get("nurse_mname")
+            nurse_lname = request.form.get("nurse_lname")
+
+            room_type = request.form.get("n_room_type")
+
+            
+
+            rooms = db.execute("select type from room")
+            rooms = [list(row) for row in rooms]
+            rooms = [str(item[0]) for item in rooms]
+
+            nurses = db.execute("select n_id from nurse where fname = '"+nurse_fname+"' and mname = '"+nurse_mname+"' and lname = '"+nurse_lname+"' ")
+            nurses = [list(row)[0] for row in nurses]
+
+            if room_type not in rooms:
+                data = dict()
+                data["room_not_exist"] = 1
+                return render_template("room_nurse_assign.html",data = data)
+            elif len(nurses)==0:
+                data = dict()
+                data["nurse_not_exist"] = 1
+                return render_template("room_nurse_assign.html",data = data)
+            else:
+                data = dict()
+                data["room_not_exist"] = 0
+                data["nurse_not_exist"] = 0
+                r_id = db.execute("select room_id from room where type = '"+room_type+"'")
+                r_id = [str(list(row)[0]) for row in r_id]
+                r_id = r_id[0]
+                n_id = nurses[0]
+
+                db.execute("update nurse set room_id = '"+ r_id +"' where n_id = '"+ n_id+"'")
+                return render_template("room_nurse_assign.html",data =data)
+                
+
+
+
 
     return app
